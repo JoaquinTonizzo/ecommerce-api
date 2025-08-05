4bc29bfdf0836101ccd4c0343fcf3fee4af7773c
Joaquin Tonizzo <joaquintonizzo1@gmail.com>
2025-08-05 17:05:41 -0300

    integrate Tailwind CSS and refactor Navbar component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { CartProvider } from './context/CartContext.jsx';
-import './styles/global.css';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
