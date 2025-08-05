2c0c43aef49e74fc427be9fff1057e2143e65797
Joaquin Tonizzo <joaquintonizzo1@gmail.com>
2025-08-05 14:57:02 -0300

    Add authentication and registration features with routing and guards

--- frontend-ecommerce/src/app/pages/login/login.ts
+++ frontend-ecommerce/src/app/pages/login/login.ts
@@ -2,12 +2,10 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-login',
   standalone: true,
   imports: [RouterLink],
-  templateUrl: './home.html',
-  styleUrl: './home.css'
+  templateUrl: './login.html',
+  styleUrl: './login.css'
 })
-export class Home {
-
-}
+export class Login {}
--- frontend-ecommerce/src/app/pages/productos/productos.ts
+++ frontend-ecommerce/src/app/pages/productos/productos.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-productos',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './productos.html',
   styleUrl: './productos.css'
 })
 export class Productos {
-
+  constructor(public auth: AuthService) {}
+  // Aquí iría la lógica de productos y agregar al carrito
 }
